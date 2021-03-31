0bc516945d015cd5c3ebf68cfde7dabe226888ac
Drejzer <drejzer@gmail.com>
2021-03-31 21:47:07 +0200

    Just joking, vegetationmap also needed some tweaking T~T

--- Sources/Scripts/VegetationMap.gd
+++ Sources/Scripts/VegetationMap.gd
@@ -1,11 +1,11 @@
 extends "res://addons/MapGenTools/_WorldMetaLayer.gd"
 
 onready var par := get_parent()
-onready var tm := par.get_node("TemperatureMap")
-onready var rfm:= par.get_node("RainfallMap")
-onready var hm := par.get_node("HeightMap")
-onready var rvm:= par.get_node("RiverMap")
-onready var drm:= par.get_node("DrainageMap")
+onready var tm := get_parent().get_node("TemperatureMap")
+onready var rfm:= get_parent().get_node("RainfallMap")
+onready var hm := get_parent().get_node("HeightMap")
+onready var rvm:= get_parent().get_node("RiverMap")
+onready var drm:= get_parent().get_node("DrainageMap")
 
 
 func Generate(args):
@@ -33,19 +33,17 @@ func Generate(args):
 		for y in range(world_y_size):
 			
 			var noiseval = osn.get_noise_3d(cos(deg)*radius+offset.x,sin(deg)*radius+offset.y,y+offset.z)+1.0
-			noiseval=(0.5+(0.4*(tm.map[x][y]-par.TemperatureLow)/(par.TemperatureHigh-par.TemperatureLow)))*noiseval+0.75*(rfm.map[x][y]-drm.map[x][y])
+			noiseval=(0.5+(0.4*(tm.map[x][y]-par.TemperatureLow)/(par.TemperatureHigh-par.TemperatureLow)))*noiseval+0.875*(rfm.map[x][y]-drm.map[x][y])
 			
-			if hm.map[x][y]>=par.MountainThreshold/2+0.5 or hm.map[x][y]<=par.SeaLevel-0.0125 or (tm.map[x][y]-par.TemperatureLow)/(par.TemperatureHigh-par.TemperatureLow)<=par.IcebergThershold*0.75 or (tm.map[x][y]>=par.TemperatureLow*0.15+par.TemperatureHigh*0.85 && rfm.map[x][y]<=0.25):
-				noiseval*=0.075
-			if rvm.map[x][y]<1&&rvm.map[x][y]>-1:
+			if hm.map[x][y]>=par.MountainThreshold/2.0+0.5 or hm.map[x][y]<=par.SeaLevel-0.0125 or (tm.map[x][y]-par.TemperatureLow)/(par.TemperatureHigh-par.TemperatureLow)<=par.IcebergThershold*0.75 or (tm.map[x][y]>=par.TemperatureLow*0.15+par.TemperatureHigh*0.85 && rfm.map[x][y]<=0.25):
+				noiseval*=0.1
+			if rvm.map[x][y]<1&&rvm.map[x][y]>-1 && !rvm.map[x][y]>0:
 				var rivcount=0
 				for i in range(3):
-					if i!=1:
 						for j in range(3):
-							if y-1+j<0 || y-1+j>=world_y_size || j==1:
+							if y-1+j<0 || y-1+j>=world_y_size:
 								pass
 							elif rvm.map[(x+world_x_size+i-1)%world_x_size][y+j-1]>0:
 								rivcount+=1
-				noiseval=clamp(noiseval+0.0875*rivcount,0.0,2.0)
+				noiseval=clamp(noiseval+0.125*rivcount,0.0,2.0)/2.0
 			map[x].append(noiseval)
-
